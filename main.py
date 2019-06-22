99ea81bd9bd04e9270741c0dc0f0d3a8d4265050
Francesco Ramoni <francesco.ramoni@email.it>
2019-06-22 03:06:46 +0200

    Events are found and relative buttons are clicked

--- main.py
+++ main.py
@@ -33,19 +33,44 @@ with open("runbase-imp-param.json") as j:
 
 # set webdriver options
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+# options.add_argument('headless')
 options.add_argument(f'user-agent={user_agent}')
+options.add_argument('incognito')
 
-# create webdriver and scrape HTML
-browser = webdriver.Chrome(chrome_options=options)
-browser.get(url)
-innerHTML = browser.execute_script("return document.body.innerHTML")
-browser.quit()
+# create a new webdriver and scrape HTML until events are found
+flag = False
+attempt = 1
+while flag == False:
+    try:
+        browser.quit()
+    except:
+        pass
+    print("Attempt ", attempt)
+    attempt += 1
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.get(url)
+    innerHTML = browser.execute_script("return document.body.innerHTML")
+    with open("content.txt", "w") as f:
+        f.write(innerHTML)
+        # look for events
+        flag = "MONDAY HEROES" in innerHTML
 
-# look for events
-tree = html.fromstring(innerHTML)
-for tag in tree.xpath('//div[@class="description-2dxZCuD"]//h3'):
-    if 'MONDAY HEROES' in tag.text_content() or 'ROAD TO YOUR BEST' in tag.text_content():
-        # send SMS alarm
-        client = Client(td['twilio_client'], td['twilio_token'])
-        client.messages.create(to=td['phone_to'], from_=td['phone_from'], body=message)
+try:
+    # click on event button
+    button_event = browser.find_element_by_xpath('//div[contains(.//div//h3, "MONDAY HEROES") and contains(., "CON spogliatoio")]/child::button')
+    button_event.click()
+    # click on event button
+    button_signup = browser.find_element_by_xpath('//*[@title="Iscriviti"]').click()
+except:
+    # print("Button not found!")
+    pass
+finally:
+    input()
+    browser.quit()
+
+# send a SMS to notify
+# for tag in tree.xpath('//div[@class="description-2dxZCuD"]//h3'):
+#     if 'MONDAY HEROES' in tag.text_content() or 'ROAD TO YOUR BEST' in tag.text_content():
+#         # send SMS alarm
+#         client = Client(td['twilio_client'], td['twilio_token'])
+#         client.messages.create(to=td['phone_to'], from_=td['phone_from'], body=message)
