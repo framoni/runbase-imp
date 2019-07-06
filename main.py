46eeaa4a7a16e25966d2342f685a623aad6fe12c
Francesco Ramoni <francesco.ramoni@email.it>
2019-07-06 12:40:35 +0200

    Bot works in headless mode. Need to return when no events are found

--- main.py
+++ main.py
@@ -13,99 +13,84 @@ Author: Francesco Ramoni
 import json
 from lxml import html
 from selenium import webdriver
+import time
 from twilio.rest import Client
+
+#-------------------------------------------------------------------------------
 
 # PARAMETERS
 
 # url to be scraped
-url = 'https://www.adidas.it/adidasrunners/community/milano'
-
+ar_url = 'https://www.adidas.it/adidasrunners/community/milano'
 # request header
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-
 # message
-message = 'Le iscrizioni agli eventi Adidas di questa settimana sono state effettuate. runbase-imp'
-
+message = 'Le iscrizioni agli eventi Adidas Runners di questa settimana sono state effettuate. runbase-imp'
 # twilio data
 with open("runbase-imp-param.json") as j:
      for line in j:
              td = json.loads(line)
-
 # set webdriver options
 options = webdriver.ChromeOptions()
-# options.add_argument('headless')
+options.add_argument('headless')
 options.add_argument(f'user-agent={user_agent}')
-# options.add_argument('incognito')
 
-# CODE
+#-------------------------------------------------------------------------------
 
-# function to scrape events page until events are found
-def scrape_for_events():
-    print("Scraping HTML... ")
-    flag = False
-    while flag == False:
-        try:
-            browser.quit()
-        except:
-            pass
-        browser = webdriver.Chrome(chrome_options=options)
-        browser.get(url)
-        innerHTML = browser.execute_script("return document.body.innerHTML")
-        flag = "MONDAY HEROES" in innerHTML
-    return browser
+# FUNCTIONS
+
+# function to scrape event url
+def scrape_event_url(event_name):
+    browser.get(ar_url)
+    event_url = browser.find_element_by_xpath('//a[contains(div//div//h3, "{}") and contains(., "SENZA spogliatoio")]'.format(event_name)).get_attribute("href")
+    return event_url
+
+# function to sign up to an event
+def event_signup(event_name, do_login):
+    print("Event: {}".format(event_name))
+    event_url = scrape_event_url(event_name)
+    # go to event page
+    browser.get(event_url)
+    # login
+    if do_login:
+        login()
+    # wait 10 seconds to bypass a UI visual bug (?) showing a second login form
+    time.sleep(10)
+    # sign up to the event
+    button_signup = browser.find_element_by_xpath('//*[@title="Iscriviti"]')
+    browser.execute_script("arguments[0].click();", button_signup)
 
 # function to login to the portal
-def login(browser):
-    print("Logging in... ")
+def login():
     # click on login button
-    button_access = browser.find_element_by_xpath('//*[@title="Accedi"]')
-    browser.execute_script("arguments[0].click();", button_access)
+    button_login = browser.find_element_by_xpath('//*[@title="Accedi"]')
+    browser.execute_script("arguments[0].click();", button_login)
     # send username, password and confirm
     browser.find_element_by_id('email').send_keys(td['email'])
     browser.find_element_by_id('password').send_keys(td['pass'])
-    button_signin = browser.find_element_by_xpath('//*[@title="Invia"]')
-    browser.execute_script("arguments[0].click();", button_signin)
-    return browser
+    button_send = browser.find_element_by_xpath('//*[@title="Invia"]')
+    browser.execute_script("arguments[0].click();", button_send)
+    return
 
-# scrape the events page
-browser = scrape_for_events()
-# login
-browser = login(browser)
+#-------------------------------------------------------------------------------
 
-print("Signing up to the events... ")
-# click on event button
-event_url = browser.find_element_by_xpath('//a[contains(div//div//h3, "MONDAY HEROES") and contains(., "SENZA spogliatoio")]').get_attribute("href")
-while True:
-    try:
-        browser.get(event_url)
-        button_signup = browser.find_element_by_xpath('//*[@title="Iscriviti"]')
-        browser.execute_script("arguments[0].click();", button_signup)
-        break
-    except Exception as e:
-         print(e)
-         continue
+# SCRAPING
 
+# create a new driver
+browser = webdriver.Chrome(chrome_options=options)
+browser.implicitly_wait(60)
+
+print("Signing up to the Adidas Runners events... ")
+
+# sign up to events of interest
+event_signup('MONDAY HEROES', True)
+event_signup('ROAD TO YOUR BEST', False)
+
+# close the driver
 browser.quit()
 
-# rescrape the events page
-browser = scrape_for_events()
-# login
-browser = login(browser)
-
-# find the second event
-event_url = browser.find_element_by_xpath('//a[contains(div//div//h3, "ROAD TO YOUR BEST") and contains(., "SENZA spogliatoio")]').get_attribute("href")
-while True:
-    try:
-        browser.get(event_url)
-        button_signup = browser.find_element_by_xpath('//*[@title="Iscriviti"]').click()
-    except:
-         continue
-    else:
-         break
-
-print("Notifying via SMS... ")
-
 # send a SMS to notify
+print("Notifying via SMS... ")
 client = Client(td['twilio_client'], td['twilio_token'])
 client.messages.create(to=td['phone_to'], from_=td['phone_from'], body=message)
 
